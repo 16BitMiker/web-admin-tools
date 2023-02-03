fa7454d42fea3b27cc1c7a66df7ad0d872fdcc02
Mike Limberger <oh-hai@miker.media>
2023-02-03 14:40:00 -0500

    added setup

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+perl -M'Term::ANSIColor'=':constants' -E 'printf qq|> %s%s |, GREEN q|press enter to setup ubuntu installer requirements...|, RESET; <STDIN>;'
+
+perl -M'Term::ANSIColor'=':constants' -sE 'map { printf qq|> %s%s\n|, YELLOW $_, RESET; system $_; } split m~`~, $cmds' -- -cmds='sudo apt update -y`sudo apt install perl -y`sudo apt install cpanminus -y`sudo cpanm Expect`sudo cpanm JSON::MaybeXS`cpanm Data::Dump' 
+
+perl -M'Term::ANSIColor'=':constants' -e 'printf qq|> %s%s\n|, GREEN q|tip: run script with sudo for best effect...|, RESET;' 
+
